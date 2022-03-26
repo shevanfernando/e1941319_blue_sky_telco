fa66e2af7a78ae0e398f6b58bd37e5eae4a09f6f
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2022-03-26 11:23:42 +0530

    initial commit

---
+++ src/main/java/com/blueskytelco/customerservice/E1941319Application.java
@@ -0,0 +1,13 @@
+package com.blueskytelco.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1941319Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E1941319Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/blueskytelco/customerservice/E1941319ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueskytelco.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1941319ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
